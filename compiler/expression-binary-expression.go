0505268a6e4d059df7f467f9ac6f9a66566d0b6a
seif-baazouzi <baazouziseif9@gmail.com>
2023-07-11 06:01:39 +0100

    concat strings using + operation

--- compiler/expression-binary-expression.go
+++ compiler/expression-binary-expression.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"os"
 	i "spl/instructions"
 	"spl/lexer"
@@ -18,10 +19,7 @@ func (b *BinaryExpression) Evaluate(env *Environment) NodeResult {
 	left := b.Left.Evaluate(env)
 	right := b.Right.Evaluate(env)
 
-	if right.Type != IntType || left.Type != IntType {
-		logs.PrintError(b.Op, "Type Error: binary operation can only implied on numerical types")
-		os.Exit(1)
-	}
+	checkBinaryExpressionTypes(left, right, b.Op)
 
 	code := []string{
 		right.Assembly,
@@ -32,16 +30,24 @@ func (b *BinaryExpression) Evaluate(env *Environment) NodeResult {
 
 	switch b.Op.Type {
 	case lexer.PLUS:
-		code = append(code, i.Add("rax", "rbx"))
+		if left.Type == StrType {
+			code = append(code, concatStrings(left, right))
+		} else {
+			code = append(code, i.Add("rax", "rbx"))
+		}
+
 	case lexer.MINUS:
 		code = append(code, i.Sub("rax", "rbx"))
+
 	case lexer.MULTIPLY:
 		code = append(code, i.Mul("rbx"))
+
 	case lexer.DIVIDE:
 		code = append(code, strings.Join([]string{
 			i.Xor("rdx", "rdx"),
 			i.Div("rbx"),
 		}, "\n"))
+
 	case lexer.MODULO:
 		code = append(code, strings.Join([]string{
 			i.Xor("rdx", "rdx"),
@@ -50,5 +56,81 @@ func (b *BinaryExpression) Evaluate(env *Environment) NodeResult {
 		}, "\n"))
 	}
 
-	return NodeResult{Type: IntType, Assembly: strings.Join(code, "\n")}
+	return NodeResult{Type: left.Type, Assembly: strings.Join(code, "\n")}
+}
+
+func checkBinaryExpressionTypes(left NodeResult, right NodeResult, op lexer.Token) {
+	if right.Type == IntType && left.Type == IntType {
+		return
+	}
+
+	if right.Type == StrType && left.Type == StrType && op.Type == lexer.PLUS {
+		return
+	}
+
+	logs.PrintError(
+		op,
+		fmt.Sprintf(
+			"Type Error: can not do operation %s on types %s and %s",
+			op.Symbol,
+			left.Type.ToString(),
+			right.Type.ToString(),
+		),
+	)
+	os.Exit(1)
+}
+
+func concatStrings(left NodeResult, right NodeResult) string {
+	return strings.Join([]string{
+		i.Mov("rax", "[rax]"),
+		i.Mov("rbx", "[rbx]"),
+
+		i.Mov("rdx", "rax"),
+
+		// calc new str length
+		i.Mov("rcx", "[rdx]"),
+		i.Add("rcx", "[rbx]"),
+
+		// allocate new str
+		i.Add("rcx", "8"),
+		i.Push("rcx"),
+		i.Call("_allocate"),
+		i.Add("rsp", "8"),
+		i.Sub("rcx", "8"),
+
+		// set new str length
+		i.Mov("[rax]", "rcx"),
+
+		// copy first str
+		i.Mov("rcx", "rdx"),
+		i.Add("rcx", "8"),
+		i.Push("rcx"),
+		i.Mov("rcx", "rax"),
+		i.Add("rcx", "8"),
+		i.Push("rcx"),
+		i.Mov("rcx", "[rdx]"),
+		i.Push("rcx"),
+		i.Call("_mem_copy"),
+		i.Add("rsp", "8*3"),
+
+		// copy second str
+		i.Mov("rcx", "rbx"),
+		i.Add("rcx", "8"),
+		i.Push("rcx"),
+		i.Mov("rcx", "rax"),
+		i.Add("rcx", "[rdx]"),
+		i.Add("rcx", "8"),
+		i.Push("rcx"),
+		i.Mov("rcx", "[rbx]"),
+		i.Push("rcx"),
+		i.Call("_mem_copy"),
+		i.Add("rsp", "8*3"),
+
+		// allocate str pointer
+		i.Mov("rbx", "rax"),
+		i.Push("8"),
+		i.Call("_allocate"),
+		i.Add("rsp", "8"),
+		i.Mov("[rax]", "rbx"),
+	}, "\n")
 }
